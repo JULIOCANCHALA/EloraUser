2b3d6a8d5bf839241fea1290ff3d0a94a86fab2a
JULIOCANCHALA <juliofernando1c@hotmail.com>
2015-12-16 11:30:57 -0500

    Initial commit

---
+++ app/src/main/java/com/canchala/julio/elorauser/MainActivity.java
@@ -0,0 +1,116 @@
+package com.canchala.julio.elorauser;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Firebase mRef;
+    ListView listView ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar);
+
+        Firebase.setAndroidContext(this);
+        listView = (ListView) findViewById(R.id.list);
+        mRef = new Firebase("https://eloraserver.firebaseio.com/");
+
+        Firebase productos = mRef.child("Productos");
+
+        Firebase estado = mRef.child("Estado");
+
+        estado.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String es = (String) dataSnapshot.getValue();
+
+                if(es.equals("Cerrado"))
+                {
+                    listView.setVisibility(View.INVISIBLE);
+                }
+                if(es.equals("Abierto"))
+                {
+                    listView.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        productos.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+
+                int contador = (int) snapshot.getChildrenCount();
+                int i = 0;
+                String[] produc = new String[contador];
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+
+                    mercancia merc = postSnapshot.getValue(mercancia.class);
+                    produc[i] = merc.getNombre();
+                    i++;
+                }
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, produc);
+                listView.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Toast.makeText(MainActivity.this, "Lectura fallida", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        Query query = productos;
+        query.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                String info = (String) dataSnapshot.child("nombre").getValue();
+                Toast.makeText(MainActivity.this, "El Producto " + info + " cambio", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String info = (String) dataSnapshot.child("nombre").getValue();
+                Toast.makeText(MainActivity.this, "El Producto " + info + " fue borrado", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+
+
+        });
+    }
+}
---
+++ app/src/main/java/com/canchala/julio/elorauser/mercancia.java
@@ -0,0 +1,36 @@
+package com.canchala.julio.elorauser;
+
+/**
+ * Created by Julio on 16/12/2015.
+ */
+public class mercancia {
+    private String nombre;
+    private String cantidad;
+    private String costo;
+    private String fabricante;
+    private String tipo;
+
+    public mercancia() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCantidad() {
+        return cantidad;
+    }
+
+    public String getCosto() {
+        return costo;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+}
+
